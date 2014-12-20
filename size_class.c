6a9c0f388548a65dbe018acfd09874b5e5d304df
nirdothan <nir.dothan@gmail.com>
2014-12-20 15:17:44 -0500

    added pop and remove to size_class.c

--- size_class.c
+++ size_class.c
@@ -2,7 +2,7 @@
  * size_class.c
  *
  *  Created on: Dec 19, 2014
- *      Author: student
+ *      Author: Nir Dothan 028434009
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,8 +17,8 @@ void makeSizeClass(size_class_t *sizeClass, unsigned int size) {
 }
 
 /* put a superblock between 2 exiting superblocks or as last */
-void plantSuperBlock( superblock_t *pBefore,  superblock_t *pNode,
-		 superblock_t *pAfter) {
+void plantSuperBlock(superblock_t *pBefore, superblock_t *pNode,
+		superblock_t *pAfter) {
 
 	/* node points back to before */
 	pNode->_meta._pPrvSblk = pBefore;
@@ -37,7 +37,11 @@ void plantSuperBlock( superblock_t *pBefore,  superblock_t *pNode,
 
 }
 
-void insertSuperBlock( size_class_t *sizeClass, superblock_t *superBlock) {
+/*
+ * insert a new superblock into the list
+ *
+ */
+void insertSuperBlock(size_class_t *sizeClass, superblock_t *superBlock) {
 
 	unsigned int i;
 	superblock_t *pSb, *pPrevSb;
@@ -77,7 +81,6 @@ void insertSuperBlock( size_class_t *sizeClass, superblock_t *superBlock) {
 		/* node points to existing first */
 		superBlock->_meta._pNxtSBlk = sizeClass->_SBlkList._first;
 
-
 		/* node points back to NULL */
 		superBlock->_meta._pPrvSblk = NULL;
 
@@ -95,3 +98,80 @@ void insertSuperBlock( size_class_t *sizeClass, superblock_t *superBlock) {
 	sizeClass->_SBlkList._length++;
 
 }
+
+/* pop the least full superblock from the list
+ *  would perform much better if we had a pointer to the last element (TBD) */
+superblock_t *popLastSuperblock(size_class_t *sizeClass) {
+
+	superblock_t *pSb = sizeClass->_SBlkList._first;
+
+	if (!pSb) /*list empty*/
+		return NULL;
+
+	for (; pSb->_meta._pNxtSBlk; pSb = pSb->_meta._pNxtSBlk)
+		;
+
+	pSb->_meta._pPrvSblk->_meta._pNxtSBlk = NULL;
+	pSb->_meta._pPrvSblk = NULL;
+
+	sizeClass->_SBlkList._length--;
+
+	return pSb;
+
+}
+
+/* relocate ahead:
+ * move the superblock to its updated place after becoming less full i.e. a block was freed
+ */
+void relocateSuperBlockAhead(size_class_t *sizeClass, superblock_t *superBlock) {
+	/* remove + insert is a temporary workaround -*/
+	removeSuperblock(sizeClass, superBlock);
+	insertSuperBlock(sizeClass, superBlock);
+}
+
+/* relocate backward:
+ * move the superblock to its updated place after becoming more full i.e. another block was allocated to the user
+ */
+void relocateSuperBlockBack(size_class_t *sizeClass, superblock_t *superBlock) {
+	/* remove + insert is a temporary workaround -*/
+	removeSuperblock(sizeClass, superBlock);
+	insertSuperBlock(sizeClass, superBlock);
+}
+
+/* remove  superblock from the list */
+superblock_t *removeSuperblock(size_class_t *sizeClass,
+		superblock_t *superBlock) {
+
+	superblock_t *pPrevSb = superBlock->_meta._pPrvSblk;
+	superblock_t *pNextSb = superBlock->_meta._pPrvSblk;
+
+	if (sizeClass->_SBlkList._first == superBlock) {/* it's first */
+
+		/* first points to next */
+		sizeClass->_SBlkList._first = pNextSb;
+
+		/* if next exists, it points back to NULL */
+		if (pNextSb)
+			pNextSb->_meta._pPrvSblk = NULL;
+
+	} else { /* not first */
+
+		if (pPrevSb == NULL) {
+			printf("program bug! size_class.c \n ");
+			exit(-1);
+		}
+		/* prev points to next */
+		pPrevSb->_meta._pNxtSBlk = pNextSb;
+
+		/* if next exists point back to prev */
+		if (pNextSb)
+			pNextSb->_meta._pPrvSblk = pPrevSb;
+
+	}
+
+	sizeClass->_SBlkList._length--;
+	superBlock->_meta._pNxtSBlk = superBlock->_meta._pPrvSblk = NULL;
+	return superBlock;
+
+}
+
